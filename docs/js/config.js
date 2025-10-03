a5ad73fa1a9495f3fae8c55b161426d97fbd48ed
Gustavo <gustavo.magalhaes_@hotmail.com>
2025-10-03 17:08:50 -0300

    teste

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,3 +1,3 @@
 // Altere para a URL do seu backend hospedado (ex: Render, Vercel, Heroku)
 // Exemplo: 'https://seu-backend.onrender.com'
-const BACKEND_URL = 'https://SEU_BACKEND_URL_AQUI';
+const BACKEND_URL = 'https://projeto-hamburgueria-final.onrender.com';
